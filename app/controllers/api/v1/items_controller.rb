e7733e3d985c666dc7ba82ca4fcd026721e83770
github username <github email address>
2020-09-10 09:34:50 -0400

    final presentation

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -10,4 +10,14 @@ class Api::V1::ItemsController < ApplicationController
         render json: item
     end
 
+    def update
+        item = Item.find(params[:id])
+        item.update(item_params)
+        render json: item
+    end
+
+    def item_params
+        params.require(:item).permit!
+    end
+
 end
